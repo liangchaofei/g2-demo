cdd6d4336ae333f6d76edf9d21ad29b70aaf073e
liangchaofei <chaofei_liang@126.com>
2023-08-19 22:28:31 +0800

    feat: 局部放大

--- src/pages/Part/index.tsx
+++ src/pages/Part/index.tsx
@@ -1,13 +1,41 @@
-import { PageContainer } from '@ant-design/pro-components';
-import DualAxisLineChart from '@/components/DualAxisLineChart'
+import { Chart } from '@antv/g2';
+import { useEffect } from 'react';
 
-const HomePage: React.FC = () => {
+const DualAxisLineChart = () => {
+ useEffect(() => {
+ 
+  const chart = new Chart({
+    container: 'chart-container',
+    theme: 'classic',
+    paddingLeft: 50,
+  });
+  chart
+  .line()
+  .data([
+    { year: '1991', value: 3 },
+    { year: '1992', value: 4 },
+    { year: '1993', value: 3.5 },
+    { year: '1994', value: 5 },
+    { year: '1995', value: 4.9 },
+    { year: '1996', value: 6 },
+    { year: '1997', value: 7 },
+    { year: '1998', value: 9 },
+    { year: '1999', value: 13 },
+  ])
+  .encode('x', 'year')
+  .encode('y', 'value')
+  // 局部放大
+  chart.interaction('brushFilter', true);
+  chart.render();
+ },[])
 
   return (
-    <PageContainer ghost>
-        <DualAxisLineChart />
-    </PageContainer>
+    <>
+      <div id="container"></div>
+      <div id="chart-container"></div>
+    </>
   );
 };
 
-export default HomePage;
+export default DualAxisLineChart;
+
--- src/pages/Single/index.tsx
+++ src/pages/Single/index.tsx
@@ -1,67 +1,67 @@
-import React, { useEffect } from 'react';
-import { Chart } from '@antv/g2';
-import { Canvas, Line } from '@antv/g';
-import { Renderer } from '@antv/g-canvas';
-
-const data = [
-  { year: '1991', value: 3 },
-  { year: '1992', value: 4 },
-  { year: '1993', value: 3.5 },
-  { year: '1994', value: 5 },
-  { year: '1995', value: 4.9 },
-  { year: '1996', value: 6 },
-  { year: '1997', value: 7 },
-  { year: '1998', value: 9 },
-  { year: '1999', value: 13 },
-];
+import { Canvas, CanvasEvent,Line } from '@antv/g';
+import { Renderer as CanvasRenderer  } from '@antv/g-canvas';
+import { Chart, ChartEvent } from '@antv/g2';
+import { useEffect } from 'react';
 
 const DualAxisLineChart = () => {
+ useEffect(() => {
+  const canvasRenderer = new CanvasRenderer();
 
-  // 创建 Canvas2D 渲染器
-//   const canvasRenderer = new Renderer();
+  // create a canvas
+  const canvas = new Canvas({
+    container: 'container',
+    width: 600,
+    height: 500,
+    renderer: canvasRenderer,
+  });
+  
+  // create a line
+  const line1 = new Line({
+    style: {
+      x1: 200,
+      y1: 100,
+      x2: 200,
+      y2: 200,
+      stroke: '#1890FF',
+      lineWidth: 2,
+      cursor: 'pointer',
+    },
+  });
+  
+  canvas.addEventListener(CanvasEvent.READY, () => {
+    canvas.appendChild(line1);
+  });
+  const chart = new Chart({
+    container: 'chart-container',
+    theme: 'classic',
+    paddingLeft: 50,
+  });
+  chart
+  .line()
+  .data([
+    { year: '1991', value: 3 },
+    { year: '1992', value: 4 },
+    { year: '1993', value: 3.5 },
+    { year: '1994', value: 5 },
+    { year: '1995', value: 4.9 },
+    { year: '1996', value: 6 },
+    { year: '1997', value: 7 },
+    { year: '1998', value: 9 },
+    { year: '1999', value: 13 },
+  ])
+  .encode('x', 'year')
+  .encode('y', 'value')
 
-//   // 创建画布
-//   const canvas = new Canvas({
-//     container: "app",
-//     width: 600,
-//     height: 500,
-//     renderer: canvasRenderer
-//   });
-
-//   // 创建边
-// const edge = new Line({
-//   style: {
-//     x1: 200,
-//     y1: 200,
-//     x2: 400,
-//     y2: 200,
-//     stroke: "#1890FF",
-//     lineWidth: 2
-//   }
-// });
-// canvas.appendChild(edge);
-  useEffect(() => {
- 
-
-    const chart = new Chart({
-      container: 'chart-container',
-      theme: 'classic',
-      paddingLeft: 50,
-    });
-    
-    chart
-    .line()
-    .data(data)
-    .encode('x', 'year')
-    .encode('y', 'value');
-
-
- 
+  chart.interaction('brushFilter', true);
   chart.render();
+ },[])
 
-  }, []);
-
-  return <div id="chart-container"></div>;
+  return (
+    <>
+      <div id="container"></div>
+      <div id="chart-container"></div>
+    </>
+  );
 };
 
 export default DualAxisLineChart;
